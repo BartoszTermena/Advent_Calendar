a819dc795acd3cbf60373e749cfc2770eaf09b8a
bartek <Bartek.NNAAN@wp.pl>
2018-12-20 15:00:35 +0100

    live plunker

---
+++ README.md
@@ -0,0 +1,2 @@
+Robo Advent Calendar
+Live: http://plnkr.co/edit/xBsSuhpwkGOANslVTwbh?p=preview
---
+++ js.js
@@ -0,0 +1,5 @@
+const calendarButton = document.querySelector(".btn-start");
+const calendarContainer = document.querySelector('.container');
+
+const calendarDays = 24;
+
